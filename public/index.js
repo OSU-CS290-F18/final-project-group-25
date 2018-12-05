c46e206308344625a134ab302f5d7755b1ff4ba2
Gabriel Kulp <gabrielkulp@gmail.com>
2018-12-04 22:14:12 -0800

    Added modal image popup

--- public/index.js
+++ public/index.js
@@ -3,39 +3,16 @@
  * Email: kulpga@oregonstate.edu
  */
 
-function submitPost () {
+function closeImage () {
+  var backdrop = document.getElementById("modal-backdrop");
+  backdrop.style = "display: none";
+}
 
-	var postPhoto   = document.getElementById('post-photo').value;
-	var postTitle   = document.getElementById('post-title').value.trim();
-	var postAuthor  = document.getElementById('post-author').value.trim();
-	var postContent = document.getElementById('post-content').value.trim();
+function zoomImage (src, alt) {
+  var image = document.getElementById("modal-img");
+  image.alt = alt;
+  image.src = src;
 
-	if (!postTitle || !postAuthor || !postContent) {
-		alert("Please fill required fields");
-	} else {
-
-		var postRequest = new XMLHttpRequest();
-		var requestURL = '/post/add';
-		postRequest.open('POST', requestURL);
-
-		var requestBody = JSON.stringify({
-			photo: postPhoto,
-			title: postTitle,
-			author: postAuthor,
-			content: postContent
-		});
-
-		console.log(requestBody);
-
-		postRequest.addEventListener('load', function (event) {
-			if (event.target.status === 200) {
-				window.location.href = '/';
-			} else {
-				alert('Error uploading post: ' + event.target.response);
-			}
-		});
-
-		postRequest.setRequestHeader('Content-Type', 'application/json');
-		postRequest.send(requestBody);
-	}
+  var backdrop = document.getElementById("modal-backdrop");
+  backdrop.style = "";
 }
